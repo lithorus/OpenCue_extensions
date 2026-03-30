867fb7f269190260aea196e54edaf721b475fbca
Jimmy Christensen <lithorus@gmail.com>
2026-03-30 22:07:40 +0200

    Remove deprecated Blender addon startup script

--- blender/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASEPATH=$(realpath  $(dirname $0))
-
-export BLENDER_SYSTEM_SCRIPTS="${BASEPATH}/OpenCueBlender"
-export PYTHONPATH="${BASEPATH}:${PYTHONPATH}"
-
-blender --python-use-system-env "$@"
